97cc593d0c9a9d825a9a1655459b91d984903107
h8d13 <hadrien.loge@gmail.com>
2025-12-19 15:02:46 +0100

    correct

--- ai_install.sh
+++ ai_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -O http://github.com/h8d13/archinstall-patch/archive/refs/heads/dot-cosai.tar.gz
-tar xvf dot-cosai.tar.gz && cd dot-cosai
+curl -L -O https://github.com/h8d13/archinstall-patch/archive/refs/heads/dot-cosai.tar.gz
+tar xvf dot-cosai.tar.gz && cd archinstall-patch-dot-cosai
 python -m archinstall
